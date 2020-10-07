077b7ddc5ff0c5017400dced78390cd5d2705b21
user <user@this.com>
2020-10-07 16:00:26 -0400

    Beginning of Customer Api for Day 2

---
+++ customer.api/src/test/java/com/bah/customer/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bah.customer.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
