75d7dacfd4c265962d9c5fa53dcfd1ccac647321
user <user@this.com>
2020-10-08 16:00:45 -0400

    Created data.sql and moved the Application file.

---
+++ customer.api/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO customer (id, name,email,password) VALUES (0, 'Mick', 'mickknutson@gmail.com', 'mick00')
+INSERT INTO customer (id, name,email,password) VALUES (0, 'Bruce', 'brucewayne@gmail.com', 'batman01')
+INSERT INTO customer (id, name,email,password) VALUES (0, 'Clark', 'clarkkentgmail.com', 'superman02')
